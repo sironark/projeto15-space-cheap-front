2cdc048ef72e3cea9623311fa4932dca3f3aa6c3
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-07-13 17:20:14 -0300

    fix: bug no versel ok

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,16 +1,14 @@
-import { useNavigate } from "react-router-dom";
+
 
 
 export function SignInPage(){
- const navigate = useNavigate()
+
     
-    function handleTest(e){
-        e.preventDefault();
-        navigate('/produto')
-    }
+    
+    
     return(
     <>
-    <strong onClick={handleTest}>SignIn Page</strong>    
+    <strong>SignIn Page</strong>    
     </>
     );
 }
