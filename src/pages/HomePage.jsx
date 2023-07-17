92b1f7deb2a2257a60fb2b67dec4e498d8ac99e5
MilenaMed <milenamfm@gmail.com>
2023-07-17 16:24:43 -0300

    Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
-
+import { Header } from "./Header";
 
 export function HomePage(){
 
     return(
     <>
+    <Header/>
+    
     <strong>Home Page</strong>    
     </>
     );
--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -6,7 +6,18 @@ const GlobalStyle = createGlobalStyle`
   src: url('../fonts/arcadeclassic/ARCADECLASSIC.TTF') format('truetype');
   font-weight: normal;
   font-style: normal;
+  color: #ffffff;
 }
+body{
+  background-image: url("https://i.imgur.com/MP8glav.png");
+  height:100vh;
+  width: 100%;
+  background-position:center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
+
 padding-left {
   font-family: "pixel-art";
 }
